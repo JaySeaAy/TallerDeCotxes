6fa8b51594371ea20c47133889fc5e6174559d3c
JaySeaAy <66727693+JaySeaAy@users.noreply.github.com>
2023-05-17 18:58:52 +0200

    els cotxes s'afegeixen a la bd

--- PracticaAzure/java.js
+++ PracticaAzure/java.js
@@ -51,3 +51,48 @@ displayCars();
 // Event Listener para el formulario de agregar coche
 const addCarForm = document.getElementById('addCarForm');
 addCarForm.addEventListener('submit', addCar);
+
+const sql = require('mssql');
+
+const config = {
+  user: 'jaumecalduch@gmail.com',
+  password: 'jaume822002',
+  server: 'tallerdecotxes.azurewebsites.net',
+  database: 'TallerCotxesDatabase',
+  options: {
+    encrypt: true
+  }
+};
+
+// Conectarse a la base de datos
+sql.connect(config, function(err) {
+  if (err) {
+    console.log(err);
+  }
+});
+
+document.getElementById("carForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  var carMake = document.getElementById("carMake").value;
+  var carModel = document.getElementById("carModel").value;
+
+  // Insertar el coche en la base de datos
+  const request = new sql.Request();
+  const query = `INSERT INTO Coches (Marca, Modelo) VALUES ('${carMake}', '${carModel}')`;
+  request.query(query, function(err, result) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Coche agregado a la base de datos");
+    }
+  });
+
+  var carItem = document.createElement("li");
+  carItem.textContent = carMake + " " + carModel;
+
+  document.getElementById("cars").appendChild(carItem);
+
+  document.getElementById("carMake").value = "";
+  document.getElementById("carModel").value = "";
+});
